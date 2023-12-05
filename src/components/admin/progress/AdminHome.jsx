5765c595e114cf582807dba3472672bf8d889149
suraj <iamsuraj3000@gmail.com>
2023-12-05 13:44:14 +0530

    updated

--- src/components/admin/progress/AdminHome.jsx
+++ src/components/admin/progress/AdminHome.jsx
@@ -3,14 +3,22 @@ import AddTask from '../task/AddTask'
 import CreateTeam from '../team/CreateTeam'
 import AddUserTeam from '../team/AddUserTeam'
 import AllTeam from '../team/AllTeam'
+import IconBtn from '../../common/IconBtn'
+import { logout } from '../../../services/operations/authAPI'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const AdminHome = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   return (
+
     <div className='text-richblack-5 flex flex-col mx-auto justify-center text-center'>
         <h1 className='text-5xl text-richblack-5 mx-auto'>Welcome Admin check here the updates</h1>
         
      
      <AddUserTeam/>
+     
      {/* <AllTeam/> */}
      
       
